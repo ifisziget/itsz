2c873a106cf4b0ec3e2ebb8de27f50faad5a7361
Kertvéllesy László Zsolt <kertvellesy.laszlo.zsolt@gmail.com>
2026-06-24 17:19:10 +0200

    Add files via upload

---
+++ create_admin.php
@@ -0,0 +1,3 @@
+<?php
+
+echo password_hash('SAJAT_EROS_JELSZO', PASSWORD_DEFAULT);
---
+++ pass.sql
@@ -0,0 +1,9 @@
+INSERT INTO admin_users
+(username, password, full_name, role)
+VALUES
+(
+    'admin',
+    '$2y$10$bF//MId.e6TmLjXWD/r8wO4GEhuyWaG4AcNn2XtrnDWXOp4yaJSVa',
+    'Admin',
+    'superadmin'
+);
